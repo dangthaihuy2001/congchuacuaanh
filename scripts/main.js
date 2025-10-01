f92b280a31f4bef95da15235e858a09f0b48901c
dhuy9916 <dhuy9916@gmail.com>
2025-10-01 15:42:41 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@
   const CANVASHEIGHT = 150
   const CANVASID = 'canvas'
 
-  let texts = ['ANH', 'YÊU', 'EM', 'BÉ NHÀN']
+  let texts = ['EM XINH ', 'NHƯ ', 'MỘT THIÊN THẦN', 'TRÂN TRÂN']
 
   let canvas,
     ctx,
